e1c70220b869cfdb1457bd0c3b1606e0bcb62cc2
yuanjie92 <425697230@qq.com>
2017-11-23 17:46:53 +0800

    修改一些命名不规范问题

--- smallproject_1000/src/com/shsxt/filter/LoginFilter.java
+++ smallproject_1000/src/com/shsxt/filter/LoginFilter.java
@@ -19,80 +19,66 @@ import org.springframework.web.context.support.WebApplicationContextUtils;
 import com.shsxt.model.UserModel;
 import com.shsxt.service.UserService;
 
-
-public class LoginFilter implements Filter
-{
+public class LoginFilter implements Filter {
 
 	@Override
-	public void destroy()
-	{
+	public void destroy() {
 
 	}
 
 	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException
-	{
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
 		HttpServletRequest req = (HttpServletRequest) request;
 		HttpServletResponse resp = (HttpServletResponse) response;
 		String uri = req.getRequestURI();
 
 		boolean hasLogin = true;
 		if (!(uri.contains("/login") || uri.contains("/js") || uri.contains("/register") || uri.contains("/logout")
-				|| uri.contains("/js")))
-		{
+				|| uri.contains("/js"))) {
 			HttpSession session = req.getSession();
 			UserModel user = (UserModel) session.getAttribute("userModel");
-			if (user == null)
-			{
+			if (user == null) {
 				Cookie[] cookie = req.getCookies();
 				Cookie rememberMe = null;
-				for (Cookie c : cookie)
-				{
-					if ("rememberMe".equals(c.getName()))
-					{
+				for (Cookie c : cookie) {
+					if ("rememberMe".equals(c.getName())) {
 						rememberMe = c;
 						break;
 					}
 				}
 
 				UserModel userModel = null;
-				if (rememberMe != null)
-				{
+				if (rememberMe != null) {
 					String token = rememberMe.getValue();
 					String[] str = token.split("\\(\\$\\)");
 					String name = str[0];
 					String password = str[1];
-					WebApplicationContext ctx = WebApplicationContextUtils.getRequiredWebApplicationContext(req.getServletContext());
+					WebApplicationContext ctx = WebApplicationContextUtils
+							.getRequiredWebApplicationContext(req.getServletContext());
 					UserService userService = ctx.getBean(UserService.class);
 					userModel = userService.queryUserByNameAndPassword(name, password);
 				}
 
-				if (userModel == null)
-				{
+				if (userModel == null) {
 					hasLogin = false;
-				}
-				else
-				{
+				} else {
 					session.setAttribute("userModel", userModel);
 				}
 			}
 
 		}
 
-		if (hasLogin)
-		{
+		if (hasLogin) {
 			chain.doFilter(req, resp);
-		}
-		else
-		{
+		} else {
 			resp.sendRedirect("login");
 		}
 
 	}
 
 	@Override
-	public void init(FilterConfig arg0) throws ServletException
-	{
+	public void init(FilterConfig arg0) throws ServletException {
 
 	}
 
